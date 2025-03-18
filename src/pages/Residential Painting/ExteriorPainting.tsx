fe1a0c82b30e9783daf821153489c262cbda1d2c
T.S.A <159101935+centmarde@users.noreply.github.com>
2025-03-19 04:20:28 +0800

    feat: remove unused Viewer3D import in ExteriorPainting component

--- src/pages/Residential Painting/ExteriorPainting.tsx
+++ src/pages/Residential Painting/ExteriorPainting.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Viewer3D } from '../../context/Context3d';
 import OutsideLayout from '../../layout/OutsideLayout';
 
 const ExteriorPainting: React.FC = () => {
